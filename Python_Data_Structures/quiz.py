e9e31fa0f86d3873bb6ab9ab29970aa6b374f6f2
Kiran-Watkins-97 <88346104+Kiran-Watkins-97@users.noreply.github.com>
2022-09-30 22:46:12 +0100

    Task 4: Create list

--- Python_Data_Structures/quiz.py
+++ Python_Data_Structures/quiz.py
@@ -1,6 +1,13 @@
 import random
 
 
+def get_defn_and_pop(word_list: list, word_dict: dict) -> (str, str):
+    randon_index = random.randrange(len(word_list))
+    word = word_list.pop(randon_index)  # If a value is assigned a .pop it still removes it from the list
+    definition = word_dict.get(word)
+    return word, definition
+
+
 def get_word_and_definition(rawstring: str) -> (str, str):
     word, definition = rawstring.split(",", 1)
     return word, definition
@@ -39,3 +46,9 @@ for rawstring in wd_set:
     wd_dict[wd] = defn
 
 print(wd_dict)
+
+
+# Turn the dictionary into a list using the list data constructor
+# This only prints the words (keys) and not their definitions
+wd_list = list(wd_dict)
+print(wd_list)
