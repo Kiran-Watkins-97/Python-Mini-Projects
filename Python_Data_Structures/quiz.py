a590cbdcebdaccefca40bdf2f11c30198ffc624c
Kiran-Watkins-97 <88346104+Kiran-Watkins-97@users.noreply.github.com>
2022-09-30 22:21:20 +0100

    Task 2: Use a Set

--- Python_Data_Structures/quiz.py
+++ Python_Data_Structures/quiz.py
@@ -7,3 +7,19 @@ wd_list = fh.readlines()
 # Print word list, we can see that the read lines method creates a list of each line of data
 print(wd_list)
 
+# Remove the first entry from word list as this is just a header row. Use the pop method
+# The pop() method removes the element at the specified position. Syntax: list.pop(pos)
+wd_list.pop(0)
+print(wd_list)
+
+# Create a set based off the list. A set is the same as a list but can't have duplicates
+# Both sets and lists are mutable, i.e. can be changed
+# You can cast an object to another format using functions like the below
+wd_set = set(wd_list)
+print(wd_set)
+
+# Now write this into a csv first opening a file using the open function with parameter w to write
+# And then write using write lines method
+fh = open("Vocabulary_set.csv", "w")
+fh.writelines(wd_set)
+
