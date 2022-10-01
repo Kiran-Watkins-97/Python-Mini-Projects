77493f318f27602a612c42408a03d6c50a6b417d
Kiran-Watkins-97 <88346104+Kiran-Watkins-97@users.noreply.github.com>
2022-10-01 10:03:00 +0100

    Python pandas merge sort filter (#4)
    * Task 1: Read the data in a Pandas DataFrame
    
    * Task 2: Use a filter to remove outliers in the data
    
    * Task 3: Combine the data using Pandas merge
    
    * Task 4: Sort the data by temperature and rainfall
    
    * Task 5: Use seaborn package to create a regeression plot
    
    * Testing Squash and Merge
    
    * Test 1
    
    * Test 2
    
    * Test 3
    
    * Remove test for squash and merge (#3)
    
    * Remove Test 3
    
    * Remove Test 2
    
    * Remove Test 1
    
    * Remove test for squash and merge
    
    * Remove Test 3
    
    * Remove Test 2
    
    * Remove Test 1

---
+++ Python_Pandas_Merge_Sort_Filter/task.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# Create dataframe using read_csv
+df_temp = pd.read_csv(r'tempYearly.csv')
+df_rain = pd.read_csv(r'rainYearly.csv')
+
+# Print the dataframe to check over them
+print(df_temp)
+print(df_rain)
+
+# Filter the dataframes using query method
+df_temp_f = df_temp.query('Temperature < 40 and Temperature > 0')
+df_rain_f = df_rain.query('Rainfall < 6 and Rainfall > 0')
+
+# Create scatter plots using the df method plot.scatter
+# Pandas .plot documentation: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.html
+df_temp_f.plot.scatter(x='Year', y='Temperature', label='Temperature by Year')
+df_rain_f.plot.scatter(x='Year', y='Rainfall', label='Rainfall by Year')
+
+# Merge the two dataframe using the merge function and with how set to outer
+df_merge = pd.merge(df_temp_f, df_rain_f, on='Year', how='inner')
+print(df_merge)
+
+# Sort the data by Temperature using the pandas method sort_values, default is asc
+df_merge_sort_temp_asc = df_merge.sort_values(by='Temperature')
+print(df_merge_sort_temp_asc)
+df_merge_sort_temp_desc = df_merge.sort_values(by='Temperature', ascending=False)
+print(df_merge_sort_temp_desc)
+
+# Sort the data by Rainfall using the pandas method sort_values, default is asc
+df_merge_sort_rain_asc = df_merge.sort_values(by='Rainfall')
+print(df_merge_sort_rain_asc)
+df_merge_sort_rain_desc = df_merge.sort_values(by='Rainfall', ascending=False)
+print(df_merge_sort_rain_desc)
+
+
+# Synthesize the seaborn window
+sns.set(rc={'figure.figsize': (12, 6)})
+
+# Then plot a regression using seaborn jointplot function
+sns.jointplot(data = df_merge, x='Rainfall', y='Temperature',  kind='reg')
+
+
+# Show the scatter pot using the pyplot function show
+# Note this shows all currently plotted graphs
+plt.show()
