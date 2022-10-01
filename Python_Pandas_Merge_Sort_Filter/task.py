57d6aba9897fffd9bfb1fbfb9f96b7f98ace7770
Kiran-Watkins-97 <88346104+Kiran-Watkins-97@users.noreply.github.com>
2022-10-01 09:17:57 +0100

    Task 2: Use a filter to remove outliers in the data

--- Python_Pandas_Merge_Sort_Filter/task.py
+++ Python_Pandas_Merge_Sort_Filter/task.py
@@ -11,10 +11,14 @@ df_rain = pd.read_csv(r'rainYearly.csv')
 print(df_temp)
 print(df_rain)
 
+# Filter the dataframes using query method
+df_temp_f = df_temp.query('Temperature < 40 and Temperature > 0')
+df_rain_f = df_rain.query('Rainfall < 6 and Rainfall > 0')
+
 # Create scatter plots using the df method plot.scatter
 # Pandas .plot documentation: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.html
-df_temp.plot.scatter(x='Year', y='Temperature', label='Temperature by Year')
-df_rain.plot.scatter(x='Year', y='Rainfall', label='Rainfall by Year')
+df_temp_f.plot.scatter(x='Year', y='Temperature', label='Temperature by Year')
+df_rain_f.plot.scatter(x='Year', y='Rainfall', label='Rainfall by Year')
 
 # Show the scatter pot using the pyplot function show
 # Note this shows all currently plotted graphs
